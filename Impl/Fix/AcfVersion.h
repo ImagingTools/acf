1bf1211fab7b22e4dd6872f109acb8e97b2a9f5c
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-01-12 11:13:12 +0000

    XCode projects updated.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@755 5144ca80-2444-0410-90ef-8152f95a0219

--- Impl/Fix/AcfVersion.h
+++ Impl/Fix/AcfVersion.h
@@ -7,8 +7,8 @@ namespace isvn
 
 
 enum RepositoryState{
-	RS_ORIGINAL_VERSION =  729,
-	RS_DIRTY_FLAG = 1,
+	RS_ORIGINAL_VERSION =  752,
+	RS_DIRTY_FLAG = 0,
 	RS_USE_VERSION = RS_ORIGINAL_VERSION + RS_DIRTY_FLAG
 };
 
