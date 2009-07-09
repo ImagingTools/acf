01dd27f8848f6abc153e0a4ab5d312339ca9ce15
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-07-09 14:26:15 +0000

    Some small correction in installation scripts.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@997 5144ca80-2444-0410-90ef-8152f95a0219

--- Install/Create_TechnicalDoc.bat
+++ Install/Create_TechnicalDoc.bat
@@ -1,20 +1,12 @@
 echo Creation of technical documentation...
 
-if not exist Temp goto DelTempSkip
-echo Removing old temporary files...
-rmdir /Q /S Temp
-:DelTempSkip
-
 echo Create doxyfile...
 "../bin/DebugVC8/Acf.exe" Create_Doxyfile.arx -config ../Config/AcfCore.xpc
 
-cd Temp
+cd TempExcl
 
 doxygen
 
 cd ..
-
-echo Removing temporary files...
-rmdir /Q /S Temp
 
 echo Technical documentation created
--- Install/Rebuild_VC8.bat
+++ Install/Rebuild_VC8.bat
@@ -15,12 +15,3 @@ echo Building ACF Tutorials done
 
 cd ../../../../Install
 
-echo Preparing ACF framework
-
-copy /Y %QTDIR%\bin\*.dll ..\Bin\ReleaseVC8
-copy /Y %QTDIR%\bin\*.dll ..\Bin\DebugVC8
-copy /Y ..\Bin\Info\*.xpc ..\Bin\ReleaseVC8
-copy /Y ..\Bin\Info\*.xpc ..\Bin\DebugVC8
-
-echo Preparing ACF framework done
-
