2b595003f0db5298f810a7f38295188291468653
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2009-10-29 14:40:17 +0000

    Small correction in script.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1139 5144ca80-2444-0410-90ef-8152f95a0219

--- Bin/PrepareACF.bat
+++ Bin/PrepareACF.bat
@@ -1,3 +1,4 @@
-copy /Y %QTDIR%\*.dll DebugVC8
-copy /Y %QTDIR%\*.dll.Manifest DebugVC8
-xcopy /Y %QTDIR%\plugins\imageformats DebugVC8
+copy /Y %QTDIR%\lib\*.dll DebugVC8
+copy /Y %QTDIR%\lib\*.dll.Manifest DebugVC8
+xcopy /Y %QTDIR%\plugins\imageformats\*.* DebugVC8\imageformats\*.*
+xcopy /Y %QTDIR%\plugins\iconengines\*.* DebugVC8\iconengines\*.*
