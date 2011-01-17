1cfd315136b7232a1e6d07a93449817d98a7072f
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2011-01-17 16:33:11 +0000

    Visual studio installations (VC8, VC9) includes only adequate project and binary files now.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@1663 5144ca80-2444-0410-90ef-8152f95a0219

--- Install/Create_VC_LGPL.bat
+++ Install/Create_VC_LGPL.bat
@@ -1,5 +1,5 @@
 echo off
 
 echo Copying files to Temp directory...
-"../bin/Debug%COMPILER_EXT%/Acf.exe" Create_VC_LGPL.arx -config ../Config/Core.xpc
+"../bin/Debug%COMPILER_EXT%/Acf.exe" Create_%COMPILER_EXT%_LGPL.arx -config ../Config/Core.xpc
 
