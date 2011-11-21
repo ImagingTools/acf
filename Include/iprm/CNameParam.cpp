c41cb2fa2c00c95a745a4292ff346646db2e2184
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2011-11-21 15:07:57 +0000

    Forgotten file added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2067 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/iprm/CNameParam.cpp
@@ -0,0 +1,57 @@
+#include "iprm/CNameParam.h"
+
+
+// ACF includes
+#include "istd/TChangeNotifier.h"
+
+#include "iser/IArchive.h"
+#include "iser/CArchiveTag.h"
+
+
+namespace iprm
+{
+
+
+// reimplemented (iprm::INameParam)
+
+const istd::CString& CNameParam::GetName() const
+{
+	return m_name;
+}
+
+
+void CNameParam::SetName(const istd::CString& name)
+{
+	if (m_name != name){
+		istd::CChangeNotifier changePtr(this);
+
+		m_name = name;
+	}
+}
+
+
+bool CNameParam::IsNameFixed() const
+{
+	return false;
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CNameParam::Serialize(iser::IArchive& archive)
+{
+	static iser::CArchiveTag nameTag("Name", "Object name");
+
+	istd::CChangeNotifier changePtr(archive.IsStoring()? NULL: this);
+
+	bool retVal = archive.BeginTag(nameTag);
+	retVal = retVal && archive.Process(m_name);
+	retVal = retVal && archive.EndTag(nameTag);
+
+	return retVal;
+}
+
+
+} // namespace iprm
+
+
