ce05ed00fdff5d83bba5c0994c709fb75234f5f3
sol <sol@5144ca80-2444-0410-90ef-8152f95a0219>
2012-11-16 10:49:47 +0000

    iqt2d: #109: added COrientedCircleEditorComp
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2657 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/iqt2d/COrientedCircleEditorComp.cpp
@@ -0,0 +1,100 @@
+#include "iqt2d/COrientedCircleEditorComp.h"
+
+// ACF includes
+#include "istd/TChangeNotifier.h"
+
+
+namespace iqt2d
+{
+
+
+void COrientedCircleEditorComp::UpdateModel() const
+{
+	I_ASSERT(IsGuiCreated());
+
+	i2d::COrientedCircle* objectPtr = GetObjectPtr();
+	I_ASSERT(objectPtr != NULL);
+
+	istd::CChangeNotifier notifier(NULL);
+
+	i2d::CVector2d position(XSpin->value(), YSpin->value());
+
+	if (objectPtr->GetCenter() != position){
+		notifier.SetPtr(objectPtr);
+		objectPtr->SetPosition(position);
+	}
+
+	double radius = RadiusSpin->value();
+	if (objectPtr->GetRadius() != radius){
+		notifier.SetPtr(objectPtr);
+		objectPtr->SetRadius(radius);
+	}
+
+	objectPtr->SetOrientedOutside(AxisOrientationCheckBox->checkState() == Qt::Checked);
+}
+
+
+void COrientedCircleEditorComp::OnGuiModelAttached()
+{
+	BaseClass::OnGuiModelAttached();
+
+	QObject::connect(XSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+	QObject::connect(YSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+	QObject::connect(RadiusSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+
+	if (m_unitNameAttrPtr.IsValid()){
+		PositionUnitLabel->setVisible(true);
+		RadiusUnitLabel->setText(*m_unitNameAttrPtr);
+	}
+	else{
+		PositionUnitLabel->setVisible(false);
+		RadiusUnitLabel->setVisible(false);
+	}
+}
+
+
+void COrientedCircleEditorComp::OnGuiModelDetached()
+{
+	QObject::disconnect(XSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+	QObject::disconnect(YSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+	QObject::disconnect(RadiusSpin, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
+
+	BaseClass::OnGuiModelDetached();
+}
+
+
+void COrientedCircleEditorComp::UpdateGui(int /*updateFlags*/)
+{
+	I_ASSERT(IsGuiCreated());
+
+	i2d::COrientedCircle* objectPtr = GetObjectPtr();
+	if (objectPtr != NULL){
+		const i2d::CVector2d& center = objectPtr->GetCenter();
+
+		XSpin->setValue(center.GetX());
+		YSpin->setValue(center.GetY());
+
+		RadiusSpin->setValue(objectPtr->GetRadius());
+
+		AxisOrientationCheckBox->setCheckState(objectPtr->IsOrientedOutside() ? Qt::Checked : Qt::Unchecked);
+
+		UpdateAllViews();
+	}
+}
+
+
+void COrientedCircleEditorComp::OnParamsChanged(double /*value*/)
+{
+	DoUpdateModel();
+}
+
+
+void COrientedCircleEditorComp::on_AxisOrientationCheckBox_stateChanged(int /*state*/)
+{
+	DoUpdateModel();
+}
+
+
+} // namespace iqt2d
+
+
---
+++ Include/iqt2d/COrientedCircleEditorComp.h
@@ -0,0 +1,53 @@
+#ifndef iqt2d_COrientedCircleEditorComp_included
+#define iqt2d_COrientedCircleEditorComp_included
+
+
+#include "i2d/COrientedCircle.h"
+
+#include "iview/COrientedCircleShape.h"
+
+#include "iqt2d/TShapeParamsGuiCompBase.h"
+#include "iqt2d/Generated/ui_COrientedCircleEditorComp.h"
+
+
+namespace iqt2d
+{
+
+
+class COrientedCircleEditorComp:
+	public iqt2d::TShapeParamsGuiCompBase<
+	Ui::COrientedCircleEditorComp,
+	iview::COrientedCircleShape,
+	i2d::COrientedCircle>
+{
+	Q_OBJECT
+
+public:
+	typedef iqt2d::TShapeParamsGuiCompBase<
+	Ui::COrientedCircleEditorComp,
+	iview::COrientedCircleShape,
+	i2d::COrientedCircle> BaseClass;
+
+	I_BEGIN_COMPONENT(COrientedCircleEditorComp);
+	I_END_COMPONENT;
+
+	// reimplemented (imod::IModelEditor)
+	virtual void UpdateModel() const;
+
+protected:
+	// reimplemented (iqtgui::TGuiObserverWrap)
+	virtual void OnGuiModelAttached();
+	virtual void OnGuiModelDetached();
+	virtual void UpdateGui(int updateFlags = 0);
+
+protected Q_SLOTS:
+	void OnParamsChanged(double value);
+	void on_AxisOrientationCheckBox_stateChanged(int state);
+
+};
+
+
+} // namespace iqt2d
+
+
+#endif // !iqt2d_COrientedCircleEditorComp_included
