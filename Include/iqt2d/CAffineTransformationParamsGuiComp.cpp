b1558e35f61f5659e87bac1e370f6cb1b98341ee
sol <sol@5144ca80-2444-0410-90ef-8152f95a0219>
2013-01-15 12:18:27 +0000

    Issue #121: CAffineTransformationParamsGui - added Reset button
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2750 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqt2d/CAffineTransformationParamsGuiComp.cpp
+++ Include/iqt2d/CAffineTransformationParamsGuiComp.cpp
@@ -1,48 +1,43 @@
 #include "iqt2d/CAffineTransformationParamsGuiComp.h"
 
+
 namespace iqt2d
 {
 
-	
 // public methods
 
-// reimplemented (imod::IModelEditor)
 
 void CAffineTransformationParamsGuiComp::UpdateModel() const
 {
 	Q_ASSERT(IsGuiCreated());
 
-	i2d::CAffineTransformation2d* objectPtr = GetObjectPtr();
-	Q_ASSERT(objectPtr != NULL);
-
-	i2d::CVector2d translation(TranslationSpinX->value(), TranslationSpinY->value());
-
-	istd::CChangeNotifier notifier(NULL);
-
-	if(objectPtr->GetTransformation().GetTranslation() != translation){
-		notifier.SetPtr(objectPtr);
-		objectPtr->Reset(translation);		
+	i2d::CAffineTransformation2d* objectPtr = dynamic_cast<i2d::CAffineTransformation2d*>(GetObjectPtr());
+	if (objectPtr != NULL){
+		i2d::CVector2d translation(TranslationSpinX->value(), TranslationSpinY->value());
+		if (objectPtr->GetTransformation().GetTranslation() != translation){
+			istd::CChangeNotifier notifier(objectPtr);
+			objectPtr->Reset(translation);
+		}
 	}
 }
 
 
 // protected methods
 
-// reimplemented (iqtgui::TGuiObserverWrap)
 
 void CAffineTransformationParamsGuiComp::OnGuiModelAttached()
 {
 	BaseClass::OnGuiModelAttached();
 
 	connect(TranslationSpinX, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
-	connect(TranslationSpinY, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));	
+	connect(TranslationSpinY, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
 }
 
 
 void CAffineTransformationParamsGuiComp::OnGuiModelDetached()
 {
 	disconnect(TranslationSpinX, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
-	disconnect(TranslationSpinY, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));	
+	disconnect(TranslationSpinY, SIGNAL(valueChanged(double)), this, SLOT(OnParamsChanged(double)));
 
 	BaseClass::OnGuiModelDetached();
 }
@@ -52,12 +47,12 @@ void CAffineTransformationParamsGuiComp::UpdateGui(int /*updateFlags*/)
 {
 	I_ASSERT(IsGuiCreated());
 
-	i2d::CAffineTransformation2d* objectPtr = GetObjectPtr();
+	i2d::CAffineTransformation2d* objectPtr = dynamic_cast<i2d::CAffineTransformation2d*>(GetObjectPtr());
 	if (objectPtr != NULL){
 		i2d::CVector2d translation = objectPtr->GetTransformation().GetTranslation();
 
 		TranslationSpinX->setValue(translation.GetX());
-		TranslationSpinY->setValue(translation.GetY());		
+		TranslationSpinY->setValue(translation.GetY());
 
 		UpdateAllViews();
 	}
@@ -66,9 +61,21 @@ void CAffineTransformationParamsGuiComp::UpdateGui(int /*updateFlags*/)
 
 // protected slots
 
+
 void CAffineTransformationParamsGuiComp::OnParamsChanged(double /*value*/)
 {
 	DoUpdateModel();
 }
 
+
+void CAffineTransformationParamsGuiComp::on_ResetButton_clicked(bool)
+{
+	i2d::CAffineTransformation2d* objectPtr = dynamic_cast<i2d::CAffineTransformation2d*>(GetObjectPtr());
+	if (objectPtr != NULL){
+		objectPtr->Reset();
+	}
 }
+
+
+} // namespace iqt2d
+
--- Include/iqt2d/CAffineTransformationParamsGuiComp.h
+++ Include/iqt2d/CAffineTransformationParamsGuiComp.h
@@ -4,10 +4,8 @@
 // ACF includes
 #include <iview/CAffineTransformation2dShape.h>
 #include <i2d/CAffineTransformation2d.h>
-
 #include <iqt2d/TShapeParamsGuiCompBase.h>
-
-#include "iqt2d/Generated/ui_CAffineTransformationParamsGui.h"
+#include "Generated/ui_CAffineTransformationParamsGui.h"
 
 
 namespace iqt2d
@@ -23,14 +21,11 @@ class CAffineTransformationParamsGuiComp:
 						iview::CAffineTransformation2dShape,
 						i2d::CAffineTransformation2d>
 {
-	Q_OBJECT
-
 public:
 	typedef iqt2d::TShapeParamsGuiCompBase<
-					Ui::CAffineTransformationParamsGui,
-					iview::CAffineTransformation2dShape,
-					i2d::CAffineTransformation2d> BaseClass;
-	
+	Ui::CAffineTransformationParamsGui,
+	iview::CAffineTransformation2dShape,
+	i2d::CAffineTransformation2d> BaseClass;
 
 	// reimplemented (imod::IModelEditor)
 	virtual void UpdateModel() const;
@@ -41,9 +36,11 @@ protected:
 	virtual void OnGuiModelDetached();
 	virtual void UpdateGui(int updateFlags = 0);
 
-	
-protected Q_SLOTS:
-	void OnParamsChanged(double value);	
+
+	Q_OBJECT
+	protected Q_SLOTS:
+	void OnParamsChanged(double value);
+	void on_ResetButton_clicked(bool = false);
 };
 
 
