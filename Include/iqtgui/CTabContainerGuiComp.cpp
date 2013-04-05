37e1c5f747fd0da24701d7f9a0aca141b75b7d09
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2013-04-05 15:49:34 +0000

    Tabs activators added.Changes from AM reverted (->KL).
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2869 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtgui/CTabContainerGuiComp.cpp
+++ Include/iqtgui/CTabContainerGuiComp.cpp
@@ -12,14 +12,53 @@ namespace iqtgui
 
 // protected methods
 
-// reimplemented (CContainerGuiBase)
-
-bool CTabContainerGuiComp::CreateMainGui(QSize iconSize)
+void CTabContainerGuiComp::UpdateVisualElements()
 {
 	QTabWidget* widgetPtr = GetQtWidget();
 	Q_ASSERT(widgetPtr != NULL);
+
+	int visualProvidersCount = m_slaveWidgetsVisualCompPtr.GetCount();
+
+	for (		TabToGuiIndexMap::ConstIterator iter = m_tabToGuiIndexMap.begin();
+				iter != m_tabToGuiIndexMap.end();
+				++iter){
+		int tabIndex = iter.key();
+		Q_ASSERT(tabIndex >= 0);
+		Q_ASSERT(tabIndex < widgetPtr->count());
+
+		int guiIndex = iter.value();
+		Q_ASSERT(guiIndex >= 0);
+		Q_ASSERT(guiIndex < m_slaveWidgetsCompPtr.GetCount());
+
+		QIcon tabIcon;
+		QString toolTip;
+
+		if (guiIndex < visualProvidersCount){
+			const IVisualStatusProvider* visualProviderPtr = m_slaveWidgetsVisualCompPtr[guiIndex];
+			if (visualProviderPtr != NULL){
+				tabIcon = visualProviderPtr->GetStatusIcon();
+				toolTip = visualProviderPtr->GetStatusText();
+			}
+		}
+
+		widgetPtr->setTabIcon(tabIndex, tabIcon);
+		widgetPtr->setTabToolTip(tabIndex, toolTip);
+	}
+}
+
+
+// reimplemented (iqtgui::CGuiComponentBase)
+
+void CTabContainerGuiComp::OnGuiCreated()
+{
+	BaseClass::OnGuiCreated();
+
+	Q_ASSERT(IsGuiCreated());
+
+	QTabWidget* widgetPtr = GetQtWidget();
+	Q_ASSERT(widgetPtr != NULL);
 	if (widgetPtr == NULL){
-		return false;
+		return;
 	}
 
 	if (*m_useTriangularTabsAttrPtr){
@@ -27,17 +66,228 @@ bool CTabContainerGuiComp::CreateMainGui(QSize iconSize)
 	}
 
 	widgetPtr->setTabPosition(QTabWidget::TabPosition(*m_tabOrientationAttrPtr));
-	widgetPtr->setIconSize(iconSize);
+
+	if (m_iconSizeAttrPtr.IsValid()){
+		QSize iconSize = QSize(m_iconSizeAttrPtr->GetValue(), m_iconSizeAttrPtr->GetValue());
+		if (!iconSize.isNull() && iconSize.isValid() && !iconSize.isEmpty()){
+			widgetPtr->setIconSize(iconSize);
+		}
+	}
+
+	int tabsCount = m_tabModel.GetOptionsCount();
 
 	bool isFlat = *m_flatStyleAttrPtr;
+
 	if (isFlat){
 		widgetPtr->setStyleSheet("QTabWidget::pane{border:0px none;}");
 	}
 
-	// setup the corner widget
+	for (int guiIndex = 0; guiIndex < tabsCount; guiIndex++){
+		QString tabName = m_tabNamesAttrPtr[guiIndex];
+		iqtgui::IGuiObject* guiPtr = m_slaveWidgetsCompPtr[guiIndex];
+		if ((guiPtr != NULL) && guiPtr->CreateGui(widgetPtr)){
+			QWidget* pageWidget = new QWidget(widgetPtr);
+			QVBoxLayout* pageLayout = new QVBoxLayout(pageWidget);
+
+			pageLayout->addWidget(guiPtr->GetWidget());
+
+			if (isFlat){
+				pageLayout->setContentsMargins(0, 0, 0, 0);
+			}
+
+			int tabIndex = widgetPtr->addTab(pageWidget, tabName);
+			m_tabToGuiIndexMap[tabIndex] = guiIndex;
+
+			if (		(guiIndex < m_slaveWidgetsVisualCompPtr.GetCount()) &&
+						(guiIndex < m_slaveWidgetsModelCompPtr.GetCount())){
+				imod::IModel* modelPtr = m_slaveWidgetsModelCompPtr[guiIndex];
+				if ((m_slaveWidgetsVisualCompPtr[guiIndex] != NULL) && (modelPtr != NULL)){
+					RegisterModel(modelPtr, guiIndex);
+				}
+			}
+		}
+	}
+
+	widgetPtr->setCurrentIndex(m_tabModel.GetSelectedOptionIndex());
+
+	// setup the corner widget:
 	if (m_cornerGuiCompPtr.IsValid()){
 		if (m_cornerGuiCompPtr->CreateGui(NULL)){
 			widgetPtr->setCornerWidget(m_cornerGuiCompPtr->GetWidget());
+		}
+	}
+
+	UpdateVisualElements();
+	m_tabModel.UpdateTabsState();
+}
+
+
+void CTabContainerGuiComp::OnGuiDestroyed()
+{
+	Q_ASSERT(m_slaveWidgetsCompPtr.IsValid());
+
+	m_tabToGuiIndexMap.clear();
+
+	if (m_slaveWidgetsCompPtr.IsValid()){
+		int slaveWidgetsCount = m_slaveWidgetsCompPtr.GetCount();
+		for (int widgetIndex = 0; widgetIndex < slaveWidgetsCount; widgetIndex++){
+			iqtgui::IGuiObject* guiPtr = m_slaveWidgetsCompPtr[widgetIndex];
+			if (guiPtr != NULL){
+				guiPtr->DestroyGui();
+			}
+		}
+	}
+
+	BaseClass::OnGuiDestroyed();
+}
+
+
+// reimplemented (icomp::CComponentBase)
+
+void CTabContainerGuiComp::OnComponentCreated()
+{
+	BaseClass::OnComponentCreated();
+
+	m_tabModel.SetParent(this);
+}
+
+
+void CTabContainerGuiComp::OnComponentDestroyed()
+{
+	m_tabModel.SetParent(NULL);
+
+	BaseClass::OnComponentDestroyed();
+}
+
+
+// reimplemented (imod::CMultiModelDispatcherBase)
+
+void CTabContainerGuiComp::OnModelChanged(int /*modelId*/, int /*changeFlags*/, istd::IPolymorphic* /*updateParamsPtr*/)
+{
+	if (IsGuiCreated()){
+		UpdateVisualElements();
+	}
+}
+
+
+// public methods of the embedded class TabModel
+
+CTabContainerGuiComp::TabModel::TabModel()
+:	m_parentPtr(NULL)
+{
+	m_selectedOptionIndex = 0;
+
+	BaseClass::SetSelectionConstraints(this);
+}
+
+
+void CTabContainerGuiComp::TabModel::SetParent(CTabContainerGuiComp* parentPtr)
+{
+	if (parentPtr != m_parentPtr){
+		if (m_parentPtr != NULL){
+			EnsureModelsDetached();
+		}
+
+		if (parentPtr != NULL){
+			int activatorsCount = qMin(parentPtr->m_tabNamesAttrPtr.GetCount(), parentPtr->m_tabActivatorsModelCompPtr.GetCount());
+			for (int tabIndex = 0; tabIndex < activatorsCount; ++tabIndex){
+				imod::IModel* modelPtr = parentPtr->m_tabActivatorsModelCompPtr[tabIndex];
+
+				modelPtr->AttachObserver(this);
+			}
+		}
+
+		m_parentPtr = parentPtr;
+	}
+}
+
+
+void CTabContainerGuiComp::TabModel::UpdateTabsState()
+{
+	Q_ASSERT(m_parentPtr != NULL);
+
+	QTabWidget* tabWidgetPtr = m_parentPtr->GetQtWidget();
+	Q_ASSERT(tabWidgetPtr != NULL);
+
+	int activatorsCount = qMin(m_parentPtr->m_tabActivatorsCompPtr.GetCount(), tabWidgetPtr->count());
+	for (int i = 0; i < activatorsCount; ++i){
+		const iprm::IEnableableParam* paramPtr = m_parentPtr->m_tabActivatorsCompPtr[i];
+		if (paramPtr != NULL){
+			tabWidgetPtr->setTabEnabled(i, paramPtr->IsEnabled());
+		}
+	}
+}
+
+
+// reimplemented (iprm::ISelectionParam)
+
+bool CTabContainerGuiComp::TabModel::SetSelectedOptionIndex(int index)
+{
+	Q_ASSERT(m_parentPtr != NULL);
+
+	if (BaseClass::SetSelectedOptionIndex(index)){
+		QTabWidget* tabWidgetPtr = m_parentPtr->GetQtWidget();
+		if (tabWidgetPtr != NULL){
+			tabWidgetPtr->setCurrentIndex(index);
+		}
+	}
+
+	return false;
+}
+
+
+// reimplemented (iprm::IOptionsList)
+
+int CTabContainerGuiComp::TabModel::GetOptionsFlags() const
+{
+	return SCF_SUPPORT_DISABLED | SCF_SUPPORT_UNIQUE_ID;
+}
+
+
+int CTabContainerGuiComp::TabModel::GetOptionsCount() const
+{
+	Q_ASSERT(m_parentPtr != NULL);
+
+	return m_parentPtr->m_tabNamesAttrPtr.GetCount();
+}
+
+
+QString CTabContainerGuiComp::TabModel::GetOptionName(int index) const
+{
+	Q_ASSERT(m_parentPtr != NULL);
+	Q_ASSERT(index < m_parentPtr->m_tabNamesAttrPtr.GetCount());
+
+	return m_parentPtr->m_tabNamesAttrPtr[index];
+}
+
+
+QString CTabContainerGuiComp::TabModel::GetOptionDescription(int /*index*/) const
+{
+	return "";
+}
+
+
+QByteArray CTabContainerGuiComp::TabModel::GetOptionId(int index) const
+{
+	Q_ASSERT(m_parentPtr != NULL);
+	Q_ASSERT(index < m_parentPtr->m_tabNamesAttrPtr.GetCount());
+
+	return m_parentPtr->m_tabNamesAttrPtr[index].toUtf8();
+}
+
+
+bool CTabContainerGuiComp::TabModel::IsOptionEnabled(int index) const
+{
+	Q_ASSERT(m_parentPtr != NULL);
+
+	if (index >= m_parentPtr->m_slaveWidgetsCompPtr.GetCount()){
+		return false;
+	}
+
+	if (index < m_parentPtr->m_tabActivatorsCompPtr.GetCount()){
+		const iprm::IEnableableParam* paramPtr = m_parentPtr->m_tabActivatorsCompPtr[index];
+		if (paramPtr != NULL){
+			return paramPtr->IsEnabled();
 		}
 	}
 
@@ -45,67 +295,19 @@ bool CTabContainerGuiComp::CreateMainGui(QSize iconSize)
 }
 
 
-int CTabContainerGuiComp::CreatePageGui(const QString& name, QSize /*iconSize*/, iqtgui::IGuiObject* guiPtr)
+// protected methods of the embedded class TabModel
+
+// reimplemented (istd::IChangeable)
+
+void CTabContainerGuiComp::TabModel::OnEndChanges(int changeFlags, istd::IPolymorphic* changeParamsPtr)
 {
-	QTabWidget* widgetPtr = GetQtWidget();
-	if (guiPtr == NULL || widgetPtr == NULL){
-		return -1;
+	Q_ASSERT(m_parentPtr != NULL);
+
+	BaseClass::OnEndChanges(changeFlags, changeParamsPtr);
+
+	if (m_parentPtr->IsGuiCreated()){
+		UpdateTabsState();
 	}
-
-	if (!guiPtr->CreateGui(widgetPtr)){
-		return -1;
-	}
-
-	QWidget* pageWidget = new QWidget(widgetPtr);
-	QVBoxLayout* pageLayout = new QVBoxLayout(pageWidget);
-
-	pageLayout->addWidget(guiPtr->GetWidget());
-
-	if (*m_flatStyleAttrPtr){
-		pageLayout->setContentsMargins(0, 0, 0, 0);
-	}
-
-	return widgetPtr->addTab(pageWidget, name);
-}
-
-
-void CTabContainerGuiComp::UpdateItem(int index, const QIcon& icon, const QString& toolTip)
-{
-	QTabWidget* widgetPtr = GetQtWidget();
-	Q_ASSERT(widgetPtr != NULL);
-
-	if (widgetPtr != NULL){
-		widgetPtr->setTabIcon(index, icon);
-		widgetPtr->setTabToolTip(index, toolTip);
-	}
-}
-
-
-bool CTabContainerGuiComp::SetSelectedIndex(int index)
-{
-	QTabWidget* widgetPtr = GetQtWidget();
-	Q_ASSERT(widgetPtr != NULL);
-	if (widgetPtr == NULL){
-		return false;
-	}
-
-	widgetPtr->setCurrentIndex(index);
-
-	return true;
-}
-
-
-bool CTabContainerGuiComp::SetEnabled(int index, bool isEnabled)
-{
-	QTabWidget* widgetPtr = GetQtWidget();
-	Q_ASSERT(widgetPtr != NULL);
-	if (widgetPtr == NULL){
-		return false;
-	}
-
-	widgetPtr->setTabEnabled(index, isEnabled);
-
-	return true;
 }
 
 
--- Include/iqtgui/CTabContainerGuiComp.h
+++ Include/iqtgui/CTabContainerGuiComp.h
@@ -3,10 +3,17 @@
 
 
 // Qt includes
+#include <QtCore/QMap>
 #include <QtGui/QTabWidget>
 
 // ACF includes
-#include "iqtgui/CContainerGuiBase.h"
+#include "imod/CMultiModelDispatcherBase.h"
+#include "imod/CMultiModelBridgeBase.h"
+#include "iprm/IEnableableParam.h"
+#include "iprm/CSelectionParam.h"
+#include "iprm/COptionsManager.h"
+
+#include "iqtgui/TGuiComponentBase.h"
 
 
 namespace iqtgui
@@ -19,31 +26,102 @@ namespace iqtgui
 	Optionally, you can specify the corner widget, which will be placed beside of the last tab.
 */
 class CTabContainerGuiComp:
-			public iqtgui::TContainerGuiBase<QTabWidget>
+			public iqtgui::TGuiComponentBase<QTabWidget>,
+			protected imod::CMultiModelDispatcherBase
 {
 public:
-	typedef iqtgui::TContainerGuiBase<QTabWidget> BaseClass;
+	typedef iqtgui::TGuiComponentBase<QTabWidget> BaseClass;
 
 	I_BEGIN_COMPONENT(CTabContainerGuiComp);
+		I_REGISTER_SUBELEMENT(TabModel);
+		I_REGISTER_SUBELEMENT_INTERFACE_T(TabModel, iprm::ISelectionParam, ExtractTabModel);
+		I_REGISTER_SUBELEMENT_INTERFACE_T(TabModel, iprm::IOptionsList, ExtractTabModel);
+		I_REGISTER_SUBELEMENT_INTERFACE_T(TabModel, iser::ISerializable, ExtractTabModel);
+		I_REGISTER_SUBELEMENT_INTERFACE_T(TabModel, imod::IModel, ExtractTabModel);
+		I_REGISTER_SUBELEMENT_INTERFACE_T(TabModel, istd::IChangeable, ExtractTabModel);
+		I_ASSIGN_MULTI_0(m_slaveWidgetsCompPtr, "Guis", "Slave widgets for tab window", true);
+		I_ASSIGN_MULTI_0(m_slaveWidgetsVisualCompPtr, "GuiVisualInfos", "Provide visual information for each GUI", false);
+		I_ASSIGN_TO(m_slaveWidgetsModelCompPtr, m_slaveWidgetsVisualCompPtr, false);
+		I_ASSIGN_MULTI_0(m_tabActivatorsCompPtr, "TabActivators", "Optional activators for each tab", false);
+		I_ASSIGN_TO(m_tabActivatorsModelCompPtr, m_tabActivatorsCompPtr, false);
 		I_ASSIGN(m_cornerGuiCompPtr, "CornerWidget", "Optional corner widget", false, "CornerWidget");
+		I_ASSIGN_MULTI_0(m_tabNamesAttrPtr, "Names", "Titles for the tab", true);
+		I_ASSIGN(m_iconSizeAttrPtr, "IconSize", "Size for tab icons", false, 16);
 		I_ASSIGN(m_useTriangularTabsAttrPtr, "UseTriangularTabs", "Using triangular tab form", true, false);
 		I_ASSIGN(m_tabOrientationAttrPtr, "TabBarOrientation", "Orientation of the tab bar\n 0 - North\n 1 - South\n 2 - West\n 3 - East", true, 0);
 		I_ASSIGN(m_flatStyleAttrPtr, "FlatTabs", "Flat style of the tabs", true, false);
 	I_END_COMPONENT;
 
 protected:
-	// reimplemented (CContainerGuiBase)
-	virtual bool CreateMainGui(QSize iconSize);
-	virtual int CreatePageGui(const QString& name, QSize iconSize, iqtgui::IGuiObject* guiPtr);
-	virtual void UpdateItem(int index, const QIcon& icon, const QString& toolTip);
-	virtual bool SetSelectedIndex(int index);
-	virtual bool SetEnabled(int index, bool isEnabled);
+	void UpdateVisualElements();
+
+	// reimplemented (iqtgui::CGuiComponentBase)
+	virtual void OnGuiCreated();
+	virtual void OnGuiDestroyed();
+
+	// reimplemented (icomp::CComponentBase)
+	virtual void CTabContainerGuiComp::OnComponentCreated();
+	virtual void CTabContainerGuiComp::OnComponentDestroyed();
+
+	// reimplemented (imod::CMultiModelDispatcherBase)
+	virtual void OnModelChanged(int modelId, int changeFlags, istd::IPolymorphic* updateParamsPtr);
 
 private:
+	class TabModel:
+				public iprm::CSelectionParam,
+				public imod::CMultiModelBridgeBase,
+				public iprm::IOptionsList
+	{
+	public:
+		typedef iprm::CSelectionParam BaseClass;
+
+		TabModel();
+
+		void SetParent(CTabContainerGuiComp* parentPtr);
+		void UpdateTabsState();
+
+		// reimplemented (iprm::ISelectionParam)
+		virtual bool SetSelectedOptionIndex(int index);
+
+		// reimplemented (iprm::IOptionsList)
+		virtual int GetOptionsFlags() const;
+		virtual int GetOptionsCount() const;
+		virtual QString GetOptionName(int index) const;
+		virtual QString GetOptionDescription(int index) const;
+		virtual QByteArray GetOptionId(int index) const;
+		virtual bool IsOptionEnabled(int index) const;
+
+	protected:
+		// reimplemented (istd::IChangeable)
+		virtual void OnEndChanges(int changeFlags, istd::IPolymorphic* changeParamsPtr);
+
+	private:
+		CTabContainerGuiComp* m_parentPtr;
+	};
+
+	template <class InterfaceType>
+	static InterfaceType* ExtractTabModel(CTabContainerGuiComp& component)
+	{
+		return &component.m_tabModel;
+	}
+
+private:
+	I_MULTIREF(IGuiObject, m_slaveWidgetsCompPtr);
+	I_MULTIREF(IVisualStatusProvider, m_slaveWidgetsVisualCompPtr);
+	I_MULTIREF(imod::IModel, m_slaveWidgetsModelCompPtr);
+	I_MULTIREF(iprm::IEnableableParam, m_tabActivatorsCompPtr);
+	I_MULTIREF(imod::IModel, m_tabActivatorsModelCompPtr);
 	I_REF(iqtgui::IGuiObject, m_cornerGuiCompPtr);
+	I_MULTIATTR(QString, m_tabNamesAttrPtr);
+	I_ATTR(int, m_iconSizeAttrPtr);
 	I_ATTR(bool, m_useTriangularTabsAttrPtr);
 	I_ATTR(int, m_tabOrientationAttrPtr);
 	I_ATTR(bool, m_flatStyleAttrPtr);
+
+	typedef QMap<int, int> TabToGuiIndexMap;
+	TabToGuiIndexMap m_tabToGuiIndexMap;
+
+	imod::TModelWrap<TabModel> m_tabModel;
 };
 
 
