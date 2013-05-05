85b12a827888f6e9c016f26e5c49ff248339f7df
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2013-05-05 16:03:43 +0000

    Build scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@2951 5144ca80-2444-0410-90ef-8152f95a0219

--- Build/BuildServer/BuildGmgAcf.cmd
+++ Build/BuildServer/BuildGmgAcf.cmd
@@ -1,6 +1,14 @@
-echo Update GmgAcf repository
+echo Update GMG ACF repository
 svn update C:\Temp\GmgAcf --username acf --password acf --non-interactive --trust-server-cert --no-auth-cache
 echo Update of GmgAcf repository finished
+
+echo Remove old GmgAcf files
+cd C:\Temp\GmgAcf\Acf
+del /s /q /a-h *
+cd C:\Temp\GmgAcf\AcfSln
+del /s /q /a-h *
+cd C:\Temp\GmgAcf\Iacf
+del /s /q /a-h *
 
 echo Create installation files
 
@@ -19,27 +27,23 @@ cd C:\Temp\Iacf\Install
 call _Create_VC9_OpenSource.bat
 xcopy Temp C:\Temp\GmgAcf\Iacf /E /Y
 
-echo Update 3rd-party libraries
-svn update C:\Temp\AcfPublic --username acf --password acf --non-interactive --trust-server-cert --no-auth-cache
-svn export C:\Temp\AcfPublic\3rdParty C:\Temp\GmgAcf\3rdParty --force --username acf --password acf --non-interactive --trust-server-cert --no-auth-cache
-
 cd C:\Temp\GmgAcf
 
 echo Delete removed files
 for /f "usebackq tokens=2*" %%i in (`svn status ^| findstr /r "^\!"`) do (
 
     svn delete "%%i %%j"
-
 )
+
+svn update C:\Temp\AcfPublic --username acf --password acf --non-interactive --trust-server-cert --no-auth-cache
+svn export C:\Temp\AcfPublic\3rdParty C:\Temp\GmgAcf\3rdParty --force --username acf --password acf --non-interactive --trust-server-cert --no-auth-cache
 
 
 echo Add new files
 for /f "usebackq tokens=2*" %%i in (`svn status ^| findstr /r "^\?"`) do (
 
     svn add "%%i %%j"
-
 )
-
 
 
 echo Commit changes to the repository
