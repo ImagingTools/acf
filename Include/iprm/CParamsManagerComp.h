ef1df960cfd7b6c5b55e95b34817522cf7f426bb
am <am@5144ca80-2444-0410-90ef-8152f95a0219>
2013-07-09 12:54:49 +0000

    Changes from Alex.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3007 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iprm/CParamsManagerComp.h
+++ Include/iprm/CParamsManagerComp.h
@@ -1,3 +1,25 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2011 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org, write info@imagingtools.de or contact
+**	by Skype to ACF_infoline for further information about the ACF.
+**
+********************************************************************************/
+
+
 #ifndef iprm_CParamsManagerComp_included
 #define iprm_CParamsManagerComp_included
 
@@ -132,7 +154,7 @@ private:
 
 	typedef istd::TDelPtr<ParamSet> ParamSetPtr;
 
-	typedef QVector<ParamSetPtr> ParamSets;
+	typedef QList<ParamSetPtr> ParamSets;
 
 	ParamSets m_paramSets;
 
