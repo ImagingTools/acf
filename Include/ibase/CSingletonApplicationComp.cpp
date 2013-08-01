553d0d9e78eb0a14233b5a7105800345acffb714
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2013-08-01 12:04:35 +0000

    Singleton application extended to support aspects of MDI framework.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3064 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ibase/CSingletonApplicationComp.cpp
+++ Include/ibase/CSingletonApplicationComp.cpp
@@ -3,6 +3,7 @@
 
 // Qt includes
 #include <QtCore/QString>
+#include <QtCore/QFileInfo>
 #if QT_VERSION >= 0x050000
 #include <QtWidgets/QApplication>
 #else
@@ -29,6 +30,8 @@ CSingletonApplicationComp::CSingletonApplicationComp()
 bool CSingletonApplicationComp::InitializeApplication(int argc, char** argv)
 {
 	if (m_isAlreadyRunning){
+		ShareDocumentsForOpening(argc, argv);
+
 		qDebug("Application already started");
 
 		return false;
@@ -45,6 +48,8 @@ bool CSingletonApplicationComp::InitializeApplication(int argc, char** argv)
 int CSingletonApplicationComp::Execute(int argc, char** argv)
 {
 	if (m_isAlreadyRunning){
+		ShareDocumentsForOpening(argc, argv);
+
 		qDebug("Application already started");
 
 		return 0;
@@ -79,6 +84,21 @@ QStringList CSingletonApplicationComp::GetApplicationArguments() const
 
 
 // protected methods
+
+// reimplemented (imod::CSingleModelObserverBase)
+
+void CSingletonApplicationComp::OnUpdate(int /*updateFlags*/, istd::IPolymorphic* /*updateParamsPtr*/)
+{
+	ibase::IRuntimeStatusProvider* objectPtr = GetObjectPtr();
+	if ((objectPtr != NULL) && objectPtr->GetRuntimeStatus() == ibase::IRuntimeStatusProvider::RS_RUNNING){
+		if (m_documentManagerCompPtr.IsValid()){
+			connect(&m_documentUpdateTimer, SIGNAL(timeout()), this, SLOT(OnUpdateDocumentList()));
+
+			m_documentUpdateTimer.start(500);
+		}
+	}
+}
+
 
 // reimplemented (icomp::CComponentBase)
 
@@ -125,11 +145,19 @@ void CSingletonApplicationComp::OnComponentCreated()
 			}
 		}
 	}
+
+	if (m_runtimeStatusProviderModelCompPtr.IsValid()){
+		m_runtimeStatusProviderModelCompPtr->AttachObserver(this);
+	}
 }
 
 
 void CSingletonApplicationComp::OnComponentDestroyed()
 {
+	m_documentUpdateTimer.stop();
+
+	EnsureModelDetached();
+
 	if (m_processData->isAttached()){
 		RunningProcessInfo* dataPtr = (RunningProcessInfo*)m_processData->data();
 		if (dataPtr->processId == QCoreApplication::applicationPid()){
@@ -140,6 +168,75 @@ void CSingletonApplicationComp::OnComponentDestroyed()
 
 	BaseClass::OnComponentDestroyed();
 }
+
+
+// private methods
+
+void CSingletonApplicationComp::ShareDocumentsForOpening(int argc, char** argv)
+{
+	// Set the list of documents need to be open by the document manager:
+	if (m_processData->lock()){
+		RunningProcessInfo* dataPtr = (RunningProcessInfo*)m_processData->data();
+		if (dataPtr != NULL){
+			memset(dataPtr->requestedDocuments, 0, sizeof(dataPtr->requestedDocuments));
+
+			for (int argIndex = 1; argIndex < argc; ++argIndex){
+				QFileInfo filePath(argv[argIndex]);
+				if (filePath.exists()){
+					int pathLength = qMin<int>(filePath.size(), MAX_DOCUMENT_PATH);
+
+					memcpy(
+								dataPtr->requestedDocuments[argIndex - 1],
+								filePath.absoluteFilePath().toUtf8().constData(),
+								pathLength); 
+				}
+			}
+		}
+	}
+
+	m_processData->unlock();
+}
+
+
+QStringList CSingletonApplicationComp::PopDocumentsForOpening() const
+{
+	QStringList retVal;
+
+	// Set the list of documents need to be open by the document manager:
+	if (m_processData->lock()){
+		RunningProcessInfo* dataPtr = (RunningProcessInfo*)m_processData->data();
+		if (dataPtr != NULL){
+			for (int documentIndex = 0; documentIndex < MAX_DOCUMENTS; ++documentIndex){
+				QString filePath(dataPtr->requestedDocuments[documentIndex]);
+
+				if (!filePath.isEmpty()){
+					retVal.push_back(filePath);
+				}
+			}
+		}
+
+		memset(dataPtr->requestedDocuments, 0, sizeof(dataPtr->requestedDocuments));
+	}
+
+	m_processData->unlock();
+
+	return retVal;
+}
+
+
+//private slots
+
+void CSingletonApplicationComp::OnUpdateDocumentList()
+{
+	Q_ASSERT(m_documentManagerCompPtr.IsValid());
+
+	QStringList documentsToOpen = PopDocumentsForOpening();
+
+	for (int documentIndex = 0; documentIndex < documentsToOpen.count(); ++documentIndex){
+		m_documentManagerCompPtr->OpenDocument(NULL, &documentsToOpen[documentIndex]);
+	}
+}
+
 
 } // namespace ibase
 
--- Include/ibase/CSingletonApplicationComp.h
+++ Include/ibase/CSingletonApplicationComp.h
@@ -4,13 +4,17 @@
 
 // Qt includes
 #include <QtCore/QSharedMemory>
+#include <QtCore/QObject>
+#include <QtCore/QTimer>
 #include <QtCore/QStringList>
 
 // ACF includes
+#include "imod/TSingleModelObserverBase.h"
 #include "icomp/CComponentBase.h"
-
 #include "ibase/IApplication.h"
 #include "ibase/IApplicationInfo.h"
+#include "idoc/IDocumentManager.h"
+#include "ibase/IRuntimeStatusProvider.h"
 
 
 namespace ibase
@@ -19,19 +23,26 @@ namespace ibase
 
 /**
 	Singleton application component.
-	This component wraps a "real" application component implementation and allows only a single execution of the application instance. 
+	This component wraps a "real" application component implementation and allows only a single execution of the application instance.
 */
 class CSingletonApplicationComp:
-			public icomp::CComponentBase, 
+			public QObject,
+			public icomp::CComponentBase,
+			protected imod::TSingleModelObserverBase<ibase::IRuntimeStatusProvider>,
 			virtual public ibase::IApplication
 {
+	Q_OBJECT
 public:
 	typedef icomp::CComponentBase BaseClass;
+	typedef imod::TSingleModelObserverBase<ibase::IRuntimeStatusProvider> BaseClass2;
 
 	I_BEGIN_COMPONENT(CSingletonApplicationComp);
 		I_REGISTER_INTERFACE(ibase::IApplication);
 		I_ASSIGN(m_applicationInfoCompPtr, "ApplicationInfo", "Application info used to identify the application in the system", false, "ApplicationInfo");
 		I_ASSIGN(m_slaveApplicationCompPtr, "Application", "Application component to be executed", true, "Application");
+		I_ASSIGN(m_documentManagerCompPtr, "DocumentManager", "Document manager used for opening of documents triggered by the shell", false, "DocumentManager");
+		I_ASSIGN(m_runtimeStatusProviderCompPtr, "RuntimeStatus", "Runtime status of the application", false, "RuntimeStatus");
+		I_ASSIGN_TO(m_runtimeStatusProviderModelCompPtr, m_runtimeStatusProviderCompPtr, false);
 	I_END_COMPONENT;
 
 	CSingletonApplicationComp();
@@ -43,17 +54,38 @@ public:
 	virtual QStringList GetApplicationArguments() const;
 
 protected:
+	// reimplemented (imod::CSingleModelObserverBase)
+	virtual void OnUpdate(int updateFlags, istd::IPolymorphic* updateParamsPtr);
+
 	// reimplemented (icomp::CComponentBase)
 	virtual void OnComponentCreated();
 	virtual void OnComponentDestroyed();
 
 private:
+	void ShareDocumentsForOpening(int argc, char** argv);
+	QStringList PopDocumentsForOpening() const;
+
+private Q_SLOTS:
+	void OnUpdateDocumentList();
+
+private:
+	/**
+		Maximal length of a document file path.
+	*/
+	static const int MAX_DOCUMENT_PATH = 1024;
+
+	/**
+		Maximal number of document, which can be opened from the shell.
+	*/
+	static const int MAX_DOCUMENTS = 100;
+
 	/**
 		Info about running application.
 	*/
 	struct RunningProcessInfo
 	{
 		qint64 processId;
+		char requestedDocuments[MAX_DOCUMENTS][MAX_DOCUMENT_PATH];
 	};
 
 	I_REF(ibase::IApplicationInfo, m_applicationInfoCompPtr);
@@ -61,6 +93,11 @@ private:
 
 	istd::TDelPtr<QSharedMemory> m_processData;
 	bool m_isAlreadyRunning;
+	QTimer m_documentUpdateTimer;
+
+	I_REF(idoc::IDocumentManager, m_documentManagerCompPtr);
+	I_REF(ibase::IRuntimeStatusProvider, m_runtimeStatusProviderCompPtr);
+	I_REF(imod::IModel, m_runtimeStatusProviderModelCompPtr);
 };
 
 
