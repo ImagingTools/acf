1acfa8e67a9b171e032cdf90f9b367acfdecf8c0
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2014-05-11 08:25:39 +0000

    Colorized output in console log.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3477 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ilog/CConsoleLogComp.cpp
+++ Include/ilog/CConsoleLogComp.cpp
@@ -7,6 +7,55 @@
 // ACF includes
 #include "istd/IInformationProvider.h"
 
+// Windows includes
+#ifdef Q_OS_WIN
+	#include <windows.h>
+	#define FOREGROUND_BLUE      0x0001 // text color contains blue.
+	#define FOREGROUND_GREEN     0x0002 // text color contains green.
+	#define FOREGROUND_RED       0x0004 // text color contains red.
+	#define FOREGROUND_INTENSITY 0x0008 // text color is intensified.
+	extern WINBASEAPI BOOL WINAPI SetConsoleTextAttribute(HANDLE hConsoleOutput, WORD wAttributes);
+#endif
+
+
+enum TextColors
+{
+	TC_DEFAULT,
+	TC_RED,
+	TC_YELLOW
+};
+
+
+void SetConsoleColor(TextColors color)
+{
+	switch (color){
+		case TC_DEFAULT:
+			#ifdef Q_OS_WIN
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+			#else
+			std::wcout << "\033[0m";
+			#endif
+			break;
+
+		case TC_RED:
+			#ifdef Q_OS_WIN
+			SetConsoleTextAttribute(GetStdHandle(STD_ERROR_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+			#else
+			std::wcerr << "\033[33m";
+			#endif
+			break;
+
+		case TC_YELLOW:
+			#ifdef Q_OS_WIN
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY);
+			#else
+			std::wcout << "\033[31m";
+			#endif
+			break;
+
+	}
+}
+
 
 namespace ilog
 {
@@ -19,9 +68,18 @@ namespace ilog
 void CConsoleLogComp::WriteText(const QString& text, istd::IInformationProvider::InformationCategory category)
 {
 	if (category >= istd::IInformationProvider::IC_ERROR){
+		SetConsoleColor(TC_RED);
+
 		std::wcerr << text.toLocal8Bit().constData();
 	}
+	else if (category == istd::IInformationProvider::IC_WARNING){
+		SetConsoleColor(TC_YELLOW);
+
+		std::wcout << text.toLocal8Bit().constData();
+	}
 	else{
+		SetConsoleColor(TC_DEFAULT);
+
 		std::wcout << text.toLocal8Bit().constData();
 	}
 }
