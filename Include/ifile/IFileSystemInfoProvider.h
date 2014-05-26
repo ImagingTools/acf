7be6407c0ce3e661c578ea6ab43592ed1ecc9f9b
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2014-05-26 11:35:13 +0000

    Interface for querying of the file system information added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3489 5144ca80-2444-0410-90ef-8152f95a0219

---
+++ Include/ifile/IFileSystemInfoProvider.h
@@ -0,0 +1,39 @@
+#ifndef ifile_IFileSystemInfoProvider_included
+#define ifile_IFileSystemInfoProvider_included
+
+
+// ACF includes
+#include "istd/IChangeable.h"
+#include "istd/CSystem.h"
+
+
+namespace ifile
+{
+
+
+/**
+	Provider of file system related informations.
+*/
+class IFileSystemInfoProvider: virtual public istd::IChangeable
+{
+public:
+	/**
+		Get the list of drives available on the local machine.
+	*/
+	virtual const iprm::IOptionsList& GetDriveList() const = 0;
+
+	/**
+		Get info about a given file drive.
+		\param driveIndex	Index of the file drive specified as index of the element inside of the list returned by GetDriveList.
+							Negative values mean that information about all available file drives will be cummulated.
+	*/
+	virtual const istd::CSystem::FileDriveInfo* GetFileDriveInfo(int driveIndex = -1) const = 0;
+};
+
+
+} // namespace ifile
+
+
+#endif // !ifile_IFileSystemInfoProvider_included
+
+
