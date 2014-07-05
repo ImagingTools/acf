410c7752c5394bbbbf2746ccbcbea456595ba0ef
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2014-07-05 14:51:09 +0000

    Settings serializer extended to allow to use ACF variables in root path.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3536 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqt/CSettingsSerializerComp.cpp
+++ Include/iqt/CSettingsSerializerComp.cpp
@@ -2,6 +2,7 @@
 
 
 // ACF includes
+#include "istd/CSystem.h"
 #include "iqt/CSettingsReadArchive.h"
 #include "iqt/CSettingsWriteArchive.h"
 
@@ -47,7 +48,11 @@ int CSettingsSerializerComp::LoadFromFile(
 
 			QSettings::Scope scope = m_scopeAttrPtr.IsValid() ? QSettings::Scope(*m_scopeAttrPtr) : QSettings::UserScope;
 
-			CSettingsReadArchive archive(companyName, applicationName, *m_rootKeyAttrPtr, scope);
+			CSettingsReadArchive archive(
+						companyName,
+						applicationName,
+						istd::CSystem::GetEnrolledPath(*m_rootKeyAttrPtr, false),
+						scope);
 
 			if (serializeblePtr->Serialize(archive)){
 				return OS_OK;
@@ -81,7 +86,7 @@ int CSettingsSerializerComp::SaveToFile(
 			CSettingsWriteArchive archive(
 						companyName,
 						applicationName,
-						*m_rootKeyAttrPtr,
+						istd::CSystem::GetEnrolledPath(*m_rootKeyAttrPtr, false),
 						scope,
 						&m_applicationInfoCompPtr->GetVersionInfo());
 
