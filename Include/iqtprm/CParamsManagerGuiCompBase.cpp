ceb3ee02939a70c26b5e35fc768a6ee424f08eae
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2014-07-14 17:37:42 +0000

    Optimization of the parameter manager rolled back.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3550 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CParamsManagerGuiCompBase.cpp
+++ Include/iqtprm/CParamsManagerGuiCompBase.cpp
@@ -406,9 +406,7 @@ void CParamsManagerGuiCompBase::UpdateTree()
 
 	ParamsTree->setCurrentItem(ParamsTree->topLevelItem(selectedIndex));
 
-	if (selectedIndex < 0){
-		UpdateParamsView(selectedIndex);
-	}
+	UpdateParamsView(selectedIndex);
 }
 
 
