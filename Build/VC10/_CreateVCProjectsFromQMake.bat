d85cb47c2455c0751a60f04036b9bd618a0e3c44
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-08-05 16:49:58 +0000

    Projects creation for VC10 added. Projects creation for VC12 corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3800 5144ca80-2444-0410-90ef-8152f95a0219

--- Build/VC10/_CreateVCProjectsFromQMake.bat
+++ Build/VC10/_CreateVCProjectsFromQMake.bat
@@ -1,16 +1,17 @@
 rem echo off
 
 set COMPILER_EXT=VC10
-set QMAKESPEC=%QTDIR%/mkspecs/win32-msvc2010
+set QMAKESPEC=%QTDIR%\mkspecs\win32-msvc2010
+set path=%path%;%QTDIR%\bin
 
-cd ..\..
+cd %~dp0\..\..
 
 echo Generating %COMPILER_EXT% projects...
-cd Build/QMake
-qmake -recursive -tp vc AcfAll.pro
-cd ../..
+cd Build\QMake
+%QTDIR%\bin\qmake -recursive -tp vc
+cd %~dp0\..\..
 
-call %ACFDIR%/Config/QMake/CopyVCProjToSubdir.js %COMPILER_EXT%
+call %ACFDIR%\Config\QMake\CopyVCProjToSubdir.js %COMPILER_EXT%
 
 
 cd Build\%COMPILER_EXT%
