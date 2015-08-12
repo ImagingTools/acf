6247a53304e42c21c3080f1d4839e618bd0cd98f
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-08-12 10:01:19 +0000

    XML serializers extended - component QtPck/CompactXmlFileSerializer added.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3817 5144ca80-2444-0410-90ef-8152f95a0219

--- Impl/QtPck/QtPck.cpp
+++ Impl/QtPck/QtPck.cpp
@@ -44,6 +44,11 @@ I_EXPORT_COMPONENT(
 		   "Save Serializer Archive Format" IM_CATEGORY(I_DATA_PERSISTENCE) IM_TAG("XML Loader File"));
 
 I_EXPORT_COMPONENT(
+		   CompactXmlFileSerializer,
+		   "File loader serializing objects compact XML format",
+		   "Save Serializer Archive Format Compact Attributes" IM_CATEGORY(I_DATA_PERSISTENCE) IM_TAG("XML Loader File"));
+
+I_EXPORT_COMPONENT(
 			ExtendedDocumentTemplate,
 			"Extended serialized document template",
 			"Serialized Template" IM_TAG("Document"));
