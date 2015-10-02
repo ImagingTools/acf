7753cc60019afeee0f0328ffb84b3ae60dcb37c2
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2015-10-02 15:19:30 +0000

    Script creation corrected - use release configuration instead of debug now.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@3918 5144ca80-2444-0410-90ef-8152f95a0219

--- Install/InternalParts/CreateScripts.bat
+++ Install/InternalParts/CreateScripts.bat
@@ -1,3 +1,3 @@
 echo Creating installation scripts...
 
-"%~dp0\..\..\Bin\Debug%COMPILER_EXT%\Acf.exe" Install/InternalParts/CreateScripts.arx -config %ACF_CONFIG_FILE% -input Install -output Install/TempExcl
+"%~dp0\..\..\Bin\Release%COMPILER_EXT%\Acf.exe" Install/InternalParts/CreateScripts.arx -config %ACF_CONFIG_FILE% -input Install -output Install/TempExcl
