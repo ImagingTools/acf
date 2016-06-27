d00d57370f4894e6295ae1fc59740cee4eb5fed3
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2016-06-27 07:43:17 +0000

    Cosmetics.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4203 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/ilog/IMessageContainer.h
+++ Include/ilog/IMessageContainer.h
@@ -24,7 +24,7 @@ namespace ilog
 class IMessageContainer: virtual public iser::ISerializable
 {
 public:
-	typedef QList<ilog::IMessageConsumer::MessagePtr> Messages;
+	typedef QList<IMessageConsumer::MessagePtr> Messages;
 
 	/**
 		Data model change notification flags.
