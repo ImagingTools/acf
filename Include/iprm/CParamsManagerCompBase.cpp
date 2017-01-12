c82936556a20f3facec2e68a40e9796eeac33ccf
wg <wg@5144ca80-2444-0410-90ef-8152f95a0219>
2017-01-12 12:23:16 +0000

    ParamsManager problem with fixed description names is fixed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4419 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iprm/CParamsManagerCompBase.cpp
+++ Include/iprm/CParamsManagerCompBase.cpp
@@ -273,7 +273,7 @@ QString CParamsManagerCompBase::GetParamsSetName(int index) const
 
 	int fixedSetsCount = m_fixedParamSetsCompPtr.GetCount();
 	if (index < fixedSetsCount){
-		int namesCount = qMin(m_fixedSetNamesAttrPtr.GetCount(), m_fixedParamSetsCompPtr.GetCount());
+		int namesCount = m_fixedSetNamesAttrPtr.GetCount();
 
 		if (index < namesCount){
 			return m_fixedSetNamesAttrPtr[index];
@@ -311,7 +311,7 @@ QString CParamsManagerCompBase::GetParamsSetDescription(int index) const
 {
 	Q_ASSERT((index >= 0) && (index < GetParamsSetsCount()));
 
-	int fixedSetsCount = qMin(m_fixedSetDescriptionsAttrPtr.GetCount(), m_fixedParamSetsCompPtr.GetCount());
+	int fixedSetsCount = m_fixedParamSetsCompPtr.GetCount();
 	if (index < fixedSetsCount){
 		int descriptionsCount = m_fixedSetDescriptionsAttrPtr.GetCount();
 
