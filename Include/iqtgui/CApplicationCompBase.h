ec40cde00898d64d2b673dd2603ebb6695bc4d65
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2017-07-30 09:57:08 +0000

    Text attributes corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4551 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtgui/CApplicationCompBase.h
+++ Include/iqtgui/CApplicationCompBase.h
@@ -69,7 +69,7 @@ private:
 	I_ATTR(double, m_splashTimeAttrPtr);
 	I_ATTR(QString, m_styleSheetAttrPtr);
 	I_ATTR(QString, m_iconPathAttrPtr);
-	I_TEXTATTR(m_titleFormatAttrPtr);
+	I_ATTR(QString, m_titleFormatAttrPtr);
 
 	bool m_useSplashScreen;
 	istd::CGeneralTimeStamp m_splashScreenTimer;
