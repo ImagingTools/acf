97cfc741e88b0cd0d7240500c3d922cb37dc051d
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2018-01-08 08:35:01 +0000

    Simplified code
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4631 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/imod/CModelBase.cpp
+++ Include/imod/CModelBase.cpp
@@ -87,8 +87,6 @@ void CModelBase::DetachObserver(IObserver* observerPtr)
 		}
 
 		if (info.state == AS_ATTACHED_UPDATING){
-			IObserver* observerPtr = findIter.key();
-
 			observerPtr->AfterUpdate(this, m_cumulatedChangeIds);
 		}
 
