a697fa57a110ef45fee9f3b2e344567714ceccae
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2020-02-03 08:08:02 +0000

    CParamsManagerGuiCompBase: Splitter deactivated for ComboBox mode.
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4840 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqtprm/CParamsManagerGuiCompBase.cpp
+++ Include/iqtprm/CParamsManagerGuiCompBase.cpp
@@ -717,10 +717,14 @@ void CParamsManagerGuiCompBase::OnGuiCreated()
 
 	if (*m_comboBoxViewAttrPtr){
 		ParamsTree->hide();
+
 		horizontalSpacer->changeSize(0,0);
+		ParamViewSplitter->setChildrenCollapsible(false);
+
 		AddButton->setToolButtonStyle(Qt::ToolButtonIconOnly);
 		RemoveButton->setToolButtonStyle(Qt::ToolButtonIconOnly);
-	} else {
+	}
+	else{
 		ParamsComboBox->hide();
 	}
 
