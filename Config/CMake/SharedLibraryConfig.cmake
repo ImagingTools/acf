c6b5b65a12fb6cf083e73271d7beafd26895a22d
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2021-05-24 20:20:59 +0000

    CMake projects corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4940 5144ca80-2444-0410-90ef-8152f95a0219

--- Config/CMake/SharedLibraryConfig.cmake
+++ Config/CMake/SharedLibraryConfig.cmake
@@ -22,36 +22,10 @@ target_sources(${PROJECT_NAME} PRIVATE ${UI_SOURCES})
 target_sources(${PROJECT_NAME} PRIVATE ${RESOURCES_FILES})
 target_sources(${PROJECT_NAME} PRIVATE ${SOURCES_FILE_AUX})
 
-#qt5_use_modules(${PROJECT_NAME} QuickWidgets)
-#find_package(qtlib Qt${QT_DEFAULT_MAJOR_VERSION}::Core REQUIRED)
-#target_link_libraries(Qt${QT_DEFAULT_MAJOR_VERSION}::QuickWidgets)
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}")
+set_target_properties(${PROJECT_NAME} PROPERTIES PREFIX "" SUFFIX ${TARGET_EXT})
 
 set(outbindir "${AUX_INCLUDE_DIR}/../../../Bin/${CMAKE_BUILD_TYPE}${TARGETNAME}/Plugins")
+set(LIBRARY_OUTPUT_PATH "${outbindir}")
 
-if(WIN32)
-	#set(CMAKE_CXX_FLAGS "${Qt5Widgets_EXECUTABLE_COMPILE_FLAGS}")
-	#set_target_properties(${PROJECT_NAME} PROPERTIES POSITION_INDEPENDENT_CODE ON)
-endif()
 
-#INCLUDEPATH += $$OUT_PWD/../../../Include
-#INCLUDEPATH += $$_PRO_FILE_PWD_/../../../Include
-#INCLUDEPATH += $$PWD/../../Impl
-#
-#TEMPLATE = lib
-#
-#DESTDIR = $$OUT_PWD/../../../Bin/$$COMPILER_DIR
-#
-#CONFIG += link_prl
-#CONFIG += shared
-#
-#!win32-msvc*{
-#	QMAKE_CFLAGS += -fPIC
-#}
-#
-#HEADERS += $$files($$_PRO_FILE_PWD_/../*.h, true)
-#SOURCES += $$files($$_PRO_FILE_PWD_/../*.cpp, true)
-#
-#isEqual(DEBUG_INFO_ENABLED_FOR_RELEASE, true){
-#	include(MsvcEnableDebugInfo.pri)
-#}
+
--- Config/CMake/WindeployQt.cmake
+++ Config/CMake/WindeployQt.cmake
@@ -7,29 +7,15 @@ get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
 
 message("_qt_bin_dir " ${_qt_bin_dir}/windeployqt.exe)
 
-function(windeployqt target)
-
-    # POST_BUILD step
-    # - after build, we have a bin/lib for analyzing qt dependencies
-    # - we run windeployqt on target and deploy Qt libs
-
+function(windeploy target listOptions listFiles)
     add_custom_command(TARGET ${target} POST_BUILD
 		COMMAND "${Qt5_DIR}/../../../bin/windeployqt.exe"
-#COMMAND "$ENV{QTDIR}/bin/windeployqt.exe"
-#COMMAND "${_qt_bin_dir}/windeployqt.exe"
-#ARGS --verbose 1 ${AUX_INCLUDE_DIR}/../../../Bin/${CMAKE_BUILD_TYPE}${TARGETNAME}
-                --verbose 1
-#                --release
-#                --no-svg
-#                --no-angle
-#                --no-opengl
-#                --no-opengl-sw
-#                --no-compiler-runtime
-#                --no-system-d3d-compiler
-#                \"$<TARGET_FILE:${target}>\"
-                \"${AUX_INCLUDE_DIR}/../../../Bin/${CMAKE_BUILD_TYPE}${TARGETNAME}\"
-
+		--verbose 1
+		${listOptions}
+		${listFiles}
+		
 		COMMENT "Deploying Qt libraries using windeployqt for compilation target '${target}' ..."
     )
-
+	message("text:  ${options} ${files}")
 endfunction()
+
