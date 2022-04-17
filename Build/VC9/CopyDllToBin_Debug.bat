5d088765f0d79253f214550a65875d75c0389bc0
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2022-04-17 06:46:18 +0000

    Removed deprecated stuff
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@4986 5144ca80-2444-0410-90ef-8152f95a0219

--- Build/VC9/CopyDllToBin_Debug.bat
+++
@@ -1,4 +0,0 @@
-copy /Y %QTDIR%\bin.debug\*.dll ..\..\Bin\DebugVC9
-copy /Y %QTDIR%\bin.debug\*.dll.Manifest ..\..\Bin\DebugVC9
-xcopy /Y %QTDIR%\plugins\imageformats\*.* ..\..\Bin\DebugVC9\imageformats\*.*
-xcopy /Y %QTDIR%\plugins\iconengines\*.* ..\..\Bin\DebugVC9\iconengines\*.*
