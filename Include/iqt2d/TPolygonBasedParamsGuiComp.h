c1c9152f24ac09374edb40980284dbaa106ac3fd
kl <kl@5144ca80-2444-0410-90ef-8152f95a0219>
2025-08-04 08:46:20 +0000

    Build fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/Acf@5416 5144ca80-2444-0410-90ef-8152f95a0219

--- Include/iqt2d/TPolygonBasedParamsGuiComp.h
+++ Include/iqt2d/TPolygonBasedParamsGuiComp.h
@@ -105,11 +105,11 @@ protected:
 	virtual void OnPasteData();
 
 	// delegated from QAbstractTableModel
-	virtual int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-	virtual int columnCount(const QModelIndex& parent = QModelIndex()) const override;
-	virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
-	virtual bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
-	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+	virtual int rowCount(const QModelIndex& parent = QModelIndex()) const;
+	virtual int columnCount(const QModelIndex& parent = QModelIndex()) const;
+	virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+	virtual bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+	virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 	virtual Qt::ItemFlags flags(const QModelIndex &index) const;
 
 	// reimplemented (iqtgui::CGuiComponentBase)
